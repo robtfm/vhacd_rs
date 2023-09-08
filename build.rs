00f60abb2cc459db77b9c264165cba252672ef61
robtfm <50659922+robtfm@users.noreply.github.com>
2023-09-08 14:51:17 +0100

    c++14

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 fn main() {
     cxx_build::bridge("src/lib.rs")
         .file("src/cxx_vhacd.cc")
+        .flag_if_supported("-std=c++14")
         .compile("vhacd_rs");
 
-
     println!("cargo:rerun-if-changed=src");
-    println!("cargo:rerun-if-changed=include");    
+    println!("cargo:rerun-if-changed=include");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -14,16 +14,21 @@ mod ffi {
 
         fn new_VHACDWrapper() -> UniquePtr<VHACDWrapper>;
         fn compute(&self, points: &[f32], indices: &[u32], params: &VHACDWrapperParams) -> u32;
-        fn get_hull(self: Pin<&mut VHACDWrapper>, i: u32, points: &mut Vec<f32>, indices: &mut Vec<u32>);
+        fn get_hull(
+            self: Pin<&mut VHACDWrapper>,
+            i: u32,
+            points: &mut Vec<f32>,
+            indices: &mut Vec<u32>,
+        );
     }
 }
 
 #[derive(Default)]
 pub struct ConvexHull {
-    pub points: Vec<[f32;3]>,
+    pub points: Vec<[f32; 3]>,
     pub indices: Vec<u32>,
-    pub min_bound: [f32;3],
-    pub max_bound: [f32;3],
+    pub min_bound: [f32; 3],
+    pub max_bound: [f32; 3],
 }
 
 struct Hulls<'a> {
@@ -47,12 +52,15 @@ impl<'a> Iterator for Hulls<'a> {
 
         let mut points = Vec::default();
         let mut ch = ConvexHull::default();
-        let mut min_bound = [f32::MAX;3];
-        let mut max_bound = [f32::MIN;3];
+        let mut min_bound = [f32::MAX; 3];
+        let mut max_bound = [f32::MIN; 3];
 
-        self.parent.wrapper.pin_mut().get_hull(self.index, &mut points, &mut ch.indices);
-        ch.points.extend(points.chunks_exact(3).map(|chunk| { 
-            let point: [f32;3] = chunk.try_into().unwrap();
+        self.parent
+            .wrapper
+            .pin_mut()
+            .get_hull(self.index, &mut points, &mut ch.indices);
+        ch.points.extend(points.chunks_exact(3).map(|chunk| {
+            let point: [f32; 3] = chunk.try_into().unwrap();
             for i in 0..3 {
                 min_bound[i] = min_bound[i].min(point[i]);
                 max_bound[i] = max_bound[i].max(point[i]);
@@ -83,12 +91,21 @@ impl VHACD {
         }
     }
 
-    pub fn compute(&mut self, points: &[[f32;3]], indices: &[u32], params: &VHACDWrapperParams) -> impl ExactSizeIterator<Item=ConvexHull> + '_ {
-        let points: Vec<f32> = points.into_iter().map(|xyz| [xyz[0], xyz[1], xyz[2]]).flatten().collect();
+    pub fn compute(
+        &mut self,
+        points: &[[f32; 3]],
+        indices: &[u32],
+        params: &VHACDWrapperParams,
+    ) -> impl ExactSizeIterator<Item = ConvexHull> + '_ {
+        let points: Vec<f32> = points
+            .into_iter()
+            .map(|xyz| [xyz[0], xyz[1], xyz[2]])
+            .flatten()
+            .collect();
         self.count = self.wrapper.compute(&points, indices, params);
         Hulls {
             parent: self,
-            index: 0
+            index: 0,
         }
     }
 }
@@ -96,11 +113,18 @@ impl VHACD {
 #[test]
 fn test() {
     let mut vhacd = VHACD::new();
-     
-    let points = vec!([0.0, 0.0, 0.0], [1.0, 1.0, 0.0], [1.0, 0.0, 0.0]);
+
+    let points = vec![[0.0, 0.0, 0.0], [1.0, 1.0, 0.0], [1.0, 0.0, 0.0]];
     let indices = vec![0, 1, 2];
 
-    let hulls = vhacd.compute(&points, &indices, 0.01);
+    let params = VHACDWrapperParams {
+        resolution: 100000,
+        error: 0.04,
+        max_hulls: 1024,
+        depth: 10,
+    };
+
+    let hulls = vhacd.compute(&points, &indices, &params);
 
     assert_eq!(hulls.len(), 1);
 
